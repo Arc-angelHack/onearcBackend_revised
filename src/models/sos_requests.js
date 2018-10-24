5c0bb168a5ca9920cf7a7ce5cff7e631cfc02d89
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2018-10-24 13:34:06 -0700

    made routes for sos_requests update, end request

--- src/models/sos_requests.js
+++ src/models/sos_requests.js
@@ -1,11 +1,16 @@
 const db = require('../db/knex')
 const { filterByLocation } = require('./helpers')
 
+const tbName = 'sos_requests'
 const joinTbs = () => {
-  return db('sos_requests')
+  return db(tbName)
     .select('sos_requests.id AS sos_id', '*')
     .join('users', 'users.id', '=', 'sos_requests.user_id')
 }
+
+////////////////
+// ANY USER CAN 
+///////////////
 
 // getAll SOS requests by giving lat, long & range (defaults to 20 miles)
 const getAll = async (query) => {
@@ -21,10 +26,24 @@ const getOne = async (reqId) => {
     .first()
 }
 
-// getAll SOS requests made by a user 
+////////////////////
+// BY A SINGLE USER
+////////////////////
+
+// getAll SOS requests made by a user (both finished and not)
 const getAllByUser = (userId) => {
   return joinTbs()
     .where('sos_requests.user_id', userId)
+}
+
+// get current sos request by a user 
+const getCurrent = async (userId) => {
+  return joinTbs()
+    .where({
+      finished: false,
+      user_id: userId
+    })
+    .first()
 }
 
 const createSOS = (userId, { street, city, state, zip, lat, long, description, finished }) => {
@@ -39,15 +58,56 @@ const createSOS = (userId, { street, city, state, zip, lat, long, description, f
     description,
     finished
   }
-
-  return db('sos_requests')
+  return db(tbName)
     .insert(bodyInsert)
     .returning('*')
+}
+
+const find = (tableName, userId, reqId) => {
+  return db(tableName)
+    .where({ user_id: userId, id: reqId })
+    .first()
+}
+
+const updateRequest = async (userId, reqId, body) => {
+  try {
+    const found = await find(tbName, userId, reqId)
+    return db(tbName)
+      .update({
+        ...found,
+        ...body,
+        updated_at: new Date()
+      })
+      .where({ id: userId })
+      .returning('*')
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const endRequest = async (userId, reqId) => {
+  try {
+    const found = await find(tbName, userId, reqId)
+    return db(tbName)
+      .update({
+        ...found,
+        finished: true
+      })
+      .where({
+        id: userId
+      })
+      .returning('*')
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 module.exports = {
   getAll,
   getAllByUser,
   getOne,
-  createSOS
+  getCurrent,
+  createSOS,
+  updateRequest,
+  endRequest
 }
--- src/routes/sos_requests.js
+++ src/routes/sos_requests.js
@@ -2,10 +2,33 @@ const router = require('express').Router()
 const ctrl = require('../controllers/sos_requests')
 const auth = require('../lib/auth')
 
+////////////////////////////////////
+// ANY USER
+///////////////////////////////////
+
 // get all sos requests in 20 miles range nearby 
 router.get('/', ctrl.getAll)
 router.get('/:reqId', ctrl.getOne)
+
+////////////////////////////////////
+// BY A SINGLE USER
+///////////////////////////////////
+
+// to see all their sos requests (both finished and not) 
 router.get('/byuser/:userId', auth.isLoggedIn, auth.isAuthorized, ctrl.getAllByUser)
+
+// to see current SOS request 
+router.get('/byuser/:userId/current', auth.isLoggedIn, auth.isAuthorized, ctrl.getCurrent)
+
+// to create SOS request
 router.post('/', auth.isLoggedIn, ctrl.createSOS)
 
+// to update SOS request 
+router.patch('/:userId/update/:reqId', auth.isLoggedIn, auth.isAuthorized, ctrl.updateRequest)
+
+// to end SOS request 
+router.patch('/:userId/end/:reqId', auth.isLoggedIn, auth.isAuthorized, ctrl.endRequest)
+
+
+// NOTE: for frontend dev, make sure user ends a current request first, before being able to make another SOS request 
 module.exports = router
